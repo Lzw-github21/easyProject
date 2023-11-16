1d35c93dbf7889a921900a9c0ea2019f36a5bbb9
—李志威- <yunbao2021@126.com>
2023-11-16 11:42:34 +0800

    代码调整

--- servers/springboot-websocket/src/main/java/cn/huanzi/qch/springbootwebsocket/AjaxTest.java
+++ servers/springboot-websocket/src/main/java/cn/huanzi/qch/springbootwebsocket/AjaxTest.java
@@ -1,6 +1,5 @@
 package cn.huanzi.qch.springbootwebsocket;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
