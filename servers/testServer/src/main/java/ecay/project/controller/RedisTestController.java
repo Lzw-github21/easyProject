86b5a38e9b064f093490a5451f276bb526951dc7
—李志威- <yunbao2021@126.com>
2023-12-12 09:26:03 +0800

    代码调整

--- servers/testServer/src/main/java/ecay/project/controller/RedisTestController.java
+++ servers/testServer/src/main/java/ecay/project/controller/RedisTestController.java
@@ -1,7 +1,10 @@
 package ecay.project.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 /**
  * @author 李志威
  * @Description
@@ -9,5 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class RedisTestController {
-
+    public static void main(String[] args) {
+        System.out.println("SELECT distinct zjk_ws_ry_jbxx.IDCard FROM( " +
+                "SELECT IDCard " +
+                "FROM zjk_ws_ry_zhuce b " +
+                "UNION ALL SELECT IDCard FROM zjk_ws_ry_xianchangrenyuan " +
+                "UNION ALL SELECT IDCard FROM zjk_ws_ry_jishugongren " +
+                "UNION ALL SELECT IDCard FROM zjk_ws_ry_zhicheng) AS JIHE " +
+                "INNER JOIN zjk_ws_ry_jbxx ON JIHE.IDCard = zjk_ws_ry_jbxx.IDCARD " +
+                "LEFT JOIN zjk_sw_qyjbxx ON zjk_ws_ry_jbxx.CORPCODE = zjk_sw_qyjbxx.CORPCODE " +
+                "WHERE 1 = 1 ");
+    }
 }
